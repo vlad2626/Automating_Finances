ddb14ff1325e97c6ccb24e3730b8bdd3a507aa96
vlad2626 <57768989+vlad2626@users.noreply.github.com>
2022-03-05 17:04:28 -0500

    Added  a file output to for totals.

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import pprint
+
 import openpyxl
 
 #global Variable
 totalSpent=0
 payments = 0
+balance = 0
+my_formatter = "{0:.2f}"
+TOTALS =\
+    {
+        'Spent': my_formatter.format(totalSpent),
+        'Paid': my_formatter.format(payments),
+        'Balance': my_formatter.format(balance),
+        'Month': 0
+    }
+
+
+def main():
+    load()
+    calculations()
+    print(balance)
+    writeFile()
+
+
 def load():
     global totalSpent
     global payments
+    global balance
     wb= openpyxl.load_workbook('Wellsfargo.xlsx')
     sheet = wb['CreditCard4']
     list(sheet.columns)[1]  # select the whole colum
@@ -16,15 +37,35 @@ def load():
            totalSpent += value
         else:
             payments += value
+
+    balance += totalSpent + payments
+    print (balance)
 # end of load
 def calculations():
-    print("total spent this month is ", totalSpent)
-    print("total paid this month is ", payments)
 
-load()
-calculations()
+    TOTALS['Spent'] = my_formatter.format(totalSpent)
+    TOTALS['Paid'] = my_formatter.format(payments)
+    TOTALS['Balance'] = my_formatter.format(balance)
+    TOTALS['Month'] = 2
+
+    print (TOTALS)
 
 
+
+
+
+#writes the output to a text file.
+def writeFile():
+
+    print('writing to a file')
+    resultFile=open('totals.txt','a')
+    resultFile.write('\nTotals =' + pprint.pformat(TOTALS))
+    resultFile.close()
+    print('Done. ')
+
+
+
+main()
 
 
 
---
+++ totals.py
@@ -0,0 +1,1 @@
+Totals ={'Balance': '-151.27', 'Month': 2, 'Paid': '2211.29', 'Spent': '-2362.56'}
